0f145e7ba9557020907003e20fa1a569d8f9c10f
Brian Bosh <bbosh@alternateinterior.com>
2022-09-30 23:01:20 -0600

    check for git and docker before installing

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # This script is used to setup the environment for the project
 ORIG_DIR=`pwd`
+
+if ! command -v git &> /dev/null
+then
+    echo "git must be available to continue"
+    exit
+fi
+
+if ! command -v docker &> /dev/null
+then
+    echo "docker must be available to continue"
+    exit
+fi
+
 mkdir ../invasion
 cp docker-compose.yml ../invasion
 cd ../invasion
